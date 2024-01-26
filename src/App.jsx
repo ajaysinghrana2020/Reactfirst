f07ecc61c27a34b07f24948e28f413c2d1c0336a
ajay <ajaysinghrana8894@gmail.com>
2024-01-26 16:57:24 +0530

    learining dimamic componets and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import reactImage from './assets/react-core-concepts.png';
-const reactDescription =['Fundamental','Crucial','Core'];
+import componetImg from './assets/components.png';
+const reactDescription = ['Fundamental', 'Crucial', 'Core'];
 
-function genRandomInt(max){
-  return Math.floor(Math.random()*(max+1));
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
 }
 function Header() {
-  return(<header>
+  return (<header>
     <img src={reactImage} alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
@@ -15,11 +16,36 @@ function Header() {
   </header>);
 }
 
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.descruption} />
+      <h3>{props.title}</h3>
+      <p>{props.descruption}</p>
+    </li>
+
+  );
+}
+
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts title="Componets" descruption=" The core UI building Block."
+            image={componetImg}  />
+            <CoreConcepts title="aj" descruption=" The core UI building Block."
+            image={componetImg}  />
+            <CoreConcepts title="pompoom" descruption=" The core UI building Block."
+            image={componetImg}  />
+            <CoreConcepts title="jaja" descruption=" The core UI building Block."
+            image={componetImg}  />
+          </ul>
+
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
