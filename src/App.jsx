6cdefed925d49f0883251d7c2a81fc1a526f55c9
ajay <ajaysinghrana8894@gmail.com>
2024-01-28 13:02:46 +0530

    state in react a meaning full topic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 
 import componetImg from './assets/components.png';
 
-import {CORE_CONCEPTS} from './data.js';
+import { useState } from 'react';
+import { CORE_CONCEPTS } from './data.js';
+// import './components/TabButton.jsx';
+import TabButton from './components/TabButton';
 
 import Header from './components/Header/Header.jsx';
 import CoreConcepts from './components/CoreConcepts';
@@ -9,6 +12,14 @@ import CoreConcepts from './components/CoreConcepts';
 
 
 function App() {
+  let [selectedTopic,setSelectedTopic ]=useState("Please click the button ");
+  let tabContent="please click a button";
+  function handleClick(selectedButton){
+    //selectedButton should be 'componet' 'jsx' 'props';
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+    // tabContent=selectedButton;
+    }
   return (
     <div>
       <Header></Header>
@@ -16,7 +27,7 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcepts {...CORE_CONCEPTS[0]}  
+            <CoreConcepts {...CORE_CONCEPTS[0]}
             />
             <CoreConcepts {...CORE_CONCEPTS[1]}
             />
@@ -24,14 +35,23 @@ function App() {
             />
             <CoreConcepts {...CORE_CONCEPTS[3]}
             />
-            
-            
-           
-            
           </ul>
 
         </section>
         <h2>Time to get started!</h2>
+
+
+        <section id="examples">
+          <h1>Examples</h1>
+          <menu>
+            <TabButton onSelect={()=>handleClick("Components")}>Component</TabButton>
+            <TabButton onSelect={()=>handleClick("Props")}>Props</TabButton>
+            <TabButton onSelect={()=>handleClick("Jsx")}>Jsx</TabButton>
+            <TabButton onSelect={()=>handleClick("State")}>State</TabButton>
+          </menu>
+          {selectedTopic}
+
+        </section>
       </main>
     </div>
   );
